f8d18fedb693aa38f694c6348e846f45d0ee1b5f
Vassili Somitko <vassili.somitko@tptlive.ee>
2022-09-28 17:33:40 +0300

    Small update

--- styles/Main.js
+++ styles/Main.js
@@ -7,8 +7,7 @@ export const Styles = StyleSheet.create({
         backgroundColor: '#FFF',
     },
 
-    Profile:
-    {
+    Profile:{
         flex: 1,
         
         ImageBorder: {
@@ -24,6 +23,43 @@ export const Styles = StyleSheet.create({
             borderRadius: 100,
             borderWidth: 5,
             borderColor: '#FFF',
+        },
+    },
+
+    Lessons: {
+        flex: 1,
+
+        Task: {
+            width: '90%',
+            height: 100,
+            backgroundColor: '#000',
+            margin: '5%',
+            borderRadius: '25%',
+            border: 1,
+
+            Text: {
+                width: '100%',
+                marginTop: 10,
+                paddingLeft: 12,
+                paddingRight: 12,
+                color: 'white',
+                fontSize: 24,
+                fontWeight: 'bolt',
+            },
+
+            Description: {
+                paddingLeft: 12,
+                paddingRight: 12,
+                color: 'white',
+                fontSize: 16,
+            },
+        },
+        TaskImage: {
+            width: '100%',
+            height: '100%',
+            borderRadius: '25%',
+            backgroundColor: '#000',
+            opacity: 0.6,
         },
     },
 });
--- tabs/Lessons.js
+++ tabs/Lessons.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, Button, Image, SafeAreaView, ScrollView, View, TouchableOpacity } from 'react-native';
+import { Text, Button, Image, SafeAreaView, ScrollView, View, ImageBackground } from 'react-native';
 import { Styles } from '../styles/Main';
 
 export default function App() {
 
   return (
     <SafeAreaView style={Styles.Main}>
-        <Text>Lessons</Text>
+        <ScrollView>
+          <ImageBackground source={{ uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Road_in_Norway.jpg/1200px-Road_in_Norway.jpg' }} style={Styles.Lessons.Task} imageStyle={Styles.Lessons.TaskImage}>
+            <Text style={Styles.Lessons.Task.Text}>Traffic as a System</Text>
+            <Text style={Styles.Lessons.Task.Description}>Basics of Traffic Rules. General Duties of the Driver. Signals Given by Driver</Text>
+          </ImageBackground>
+        </ScrollView>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
